3ce8f95f080c95f6f12bec9b517dbc5c79c385f7
Ashish-Abraham <temp200295@cet.ac.in>
2023-03-25 22:16:14 +0530

    added training functions & modules

---
+++ utils/train.py
@@ -0,0 +1,91 @@
+import torch
+import torchvision
+from torch import nn
+from models import discriminator, global_discriminator, local_discriminator
+from models import generator
+from train_funcs import _weights_init, display_progress, configure_optimizers
+
+
+# These configurations are from paper
+adv_criterion = nn.BCEWithLogitsLoss() 
+recon_criterion = nn.L1Loss() 
+lambda_recon = 100
+
+n_epochs = 20
+
+display_step = 10
+batch_size = 1
+lr = 0.002
+target_size = 256
+device = 'cuda'   # GPU is VVIMP
+
+local_input_shape = (3, 128, 128)
+global_input_shape = (3, 256, 256)
+arc = 2
+
+
+# discriminator training step
+def _disc_step(gen, disc, adversarial_criterion, real_images, conditioned_images):
+        fake_images = gen(conditioned_images).detach()
+        fake_images = nn.functional.interpolate(fake_images, size=128)
+        fake_logits = disc((fake_images, conditioned_images))
+
+        real_logits = disc((real_images, conditioned_images))
+
+        fake_loss = adversarial_criterion(fake_logits, torch.zeros_like(fake_logits))
+        real_loss = adversarial_criterion(real_logits, torch.ones_like(real_logits))
+        return (real_loss + fake_loss) / 2
+
+
+# generator training step
+def _gen_step(gen, disc, adversarial_criterion, recon_criterion, real_images, conditioned_images, lambda_recon):
+        fake_images = gen(conditioned_images).detach()
+        fake_images = nn.functional.interpolate(fake_images, size=128)
+        disc_logits = disc((fake_images, conditioned_images))
+        adversarial_loss = adversarial_criterion(disc_logits, torch.ones_like(disc_logits))
+
+        # calculate reconstruction loss
+        recon_loss = recon_criterion(fake_images, real_images)
+
+        return adversarial_loss + lambda_recon * recon_loss
+
+
+def train_GAN(gen, disc, dataloader, max_epochs, adversarial_criterion, recon_criterion, lr=0.002, lambda_recon=100):    
+ 
+        """
+        -Main training function for SketchGAN
+
+        -adversarial_loss = nn.BCEWithLogitsLoss()
+
+        -reconstruction_loss = nn.L1Loss()
+
+        """
+        optimizer_idx = 1
+        optimizer_D, optimizer_G = configure_optimizers(gen, disc, lr)
+        gen = gen.to(device)
+        disc = disc.to(device)
+        for epoch in range(max_epochs):    
+            gen.train()
+            disc.train()
+            for batch_idx, (real, condition) in enumerate(dataloader): 
+                real = real.to(device)
+                condition = condition.to(device)
+                loss = None
+                if optimizer_idx == 0:
+                    d_loss = _disc_step(gen, disc, adversarial_criterion, real, condition)
+                    optimizer_D.zero_grad()
+                    d_loss.backward(retain_graph=True)
+                    optimizer_D.step()
+                    loss = d_loss
+                    optimizer_idx = 1
+                elif optimizer_idx == 1:
+                    g_loss = _gen_step(gen, disc, adversarial_criterion, recon_criterion, real, condition, lambda_recon)
+                    optimizer_G.zero_grad()
+                    g_loss.backward(retain_graph=True)
+                    optimizer_G.step()
+                    loss = g_loss
+                    optimizer_idx = 0
+                
+                if batch_idx==0 and optimizer_idx==1:
+                    fake = gen(condition).detach()
+                    display_progress(condition[0], fake[0], real[0], loss)
---
+++ utils/train_funcs.py
@@ -0,0 +1,36 @@
+import torch
+import torchvision
+from torch import nn
+from models import discriminator, global_discriminator, local_discriminator
+from models import generator
+
+
+# initialize weights
+def _weights_init(m):
+    m = m.to(device)
+    if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+        
+
+# display training progress
+def display_progress(cond, fake, real, gen_loss, figsize=(10,5), ):
+    cond = cond.detach().cpu().permute(1, 2, 0)
+    fake = fake.detach().cpu().permute(1, 2, 0)
+    real = real.detach().cpu().permute(1, 2, 0)
+    
+    fig, ax = plt.subplots(1, 3, figsize=figsize)
+    ax[0].imshow(cond)
+    ax[2].imshow(fake)
+    ax[1].imshow(real)
+    plt.show()
+    print("gen_loss: ",gen_loss)
+
+
+# configure optimizers for training
+def configure_optimizers(gen, disc, lr):
+    gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
+    disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
+    return disc_opt, gen_opt    
